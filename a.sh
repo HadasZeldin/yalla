1cfea34285871d641ff7e2080aa1034cf82c781d
HadasZeldin <hadas2363@gmail.com>
2025-02-15 18:17:10 +0200

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,3 +2,9 @@
 #this is a comment
 
 echo "Hello ,World!"
+
+Create a Jenkins pipeline that includes the following:
+Stage 1: Echo a custom message provided by the user.
+Stage 2: Clone a Git repository from a URL provided by the user.
+Use pipeline parameters to capture user inputs for the message and the Git repository URL.
+Ensure that the pipeline is functional and outputs the expected results.
