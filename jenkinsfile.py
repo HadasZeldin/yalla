aed4011574d971bfcb52c74841fcd3f683180945
HadasZeldin <hadas2363@gmail.com>
2025-02-15 16:02:47 +0000

    Update jenkinsfile.py

--- jenkinsfile.py
+++ jenkinsfile.py
@@ -1,2 +1,15 @@
-first_name=input
-        
+pipeline{
+ any agent
+
+ parameters{
+  first_name=input("write your name here")
+  git_url=input("write your git url here")
+
+  stages{
+          stage{
+                  print (f"your name is {first_name}")
+                  print (f"your git is {git_url}")
+          }
+
+ }
+}
